019a85b7986a28bf55d80f95f285db5edeb5545b
IgnacioRodriguez <rodriguezignaciodc@gmail.com>
2022-09-21 14:55:08 -0300

    entrega JS

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function sumar(inversion, rendimiento){
-    var inversion = parseInt(document.getElementById('inversion'));
-    var rendimiento = parseInt(document.getElementById('rendimiento'));
-    document.getElementById('el-resultado').innerHTML = inversion + rendimiento;
+function calcular() {
+    let nombre = document.getElementById('nombre').value;
+    let inversion = document.getElementById('inversion').value;
+    let rendimiento = document.getElementById('rendimiento').value;
+    let años = document.getElementById('años').value;
+    let ganancias = (inversion * (rendimiento/100) * años);
+
+    document.getElementById('el-resultado').innerHTML = ganancias;
+
+    let resultadoHistorial = ganancias;
+    localStorage.setItem(nombre , resultadoHistorial);
+
+    document.getElementById('historial').innerHTML = localStorage.getItem(nombre, resultadoHistorial);
+
+}
+
+function historial(){
+
+    document.getElementById('historial').innerHTML = localStorage.getItem(nombre);
+
 }
