ba66701d00cf6fd22c161321c7993176083260f8
IgnacioRodriguez <rodriguezignaciodc@gmail.com>
2022-09-27 16:16:40 -0300

    segunda entrega JS

--- index.js
+++ index.js
@@ -4,36 +4,43 @@ function calcular() {
     let rendimiento = document.getElementById('rendimiento').value;
     let años = document.getElementById('años').value;
     let ganancias = (inversion * (rendimiento / 100) * años);
-
     document.getElementById('el-resultado').innerHTML = ganancias;
-
-    class inversionTotal {
-        constructor(nombre, inversion, rendimiento, años, ganancias) {
-            this.nombre = nombre;
-            this.inversion = inversion;
-            this.rendimiento = rendimiento;
-            this.años = años;
-            this.ganancias = ganancias;
-        }
-    }
-    const contenedorHistorial = document.getElementById("contenedorHistorial");
-    const arrayHistorial = [];
-
-    arrayHistorial.push(new inversionTotal(nombre, inversion, rendimiento, años, ganancias))
-
+    transacciones.push(new inversionTotal(nombre, inversion, rendimiento, años, ganancias))
     localStorage.clear();
-    localStorage.setItem("HitorialInversion", JSON.stringify(arrayHistorial));
+    localStorage.setItem("transactionHistory", JSON.stringify(transacciones));
+    mostrarHistorial();
+};
 
-    let transacciones = JSON.parse(localStorage.getItem("transactionHistory")) || [];
+function mostrarHistorial(){
     transacciones.forEach(item => {
         let itemHistorial = document.createElement("tr");
         itemHistorial.innerHTML = `
-        <td>${item.id}</td>
-        <td>${item.operacion}</td>
-        <td>${item.monedaOperada}</td>
-        <td>$ ${item.cantidad}</td>
-        <td>${item.precio}</td>
-    `;
+        <td> Invirtio en  ${item.nombre} /</td>
+        <td>Su inversion fe de $${item.inversion} /</td>
+        <td>Su rendimiento anual fue de %${item.rendimiento}/</td>
+        <td>Cantidad de años${item.años}/</td>
+        <td>Sus ganancias sin contar la inversion fue de $${item.ganancias}/</td>`;
         contenedorHistorial.append(itemHistorial);
-    }); 
+    });
+}
+
+class inversionTotal {
+    constructor(nombre, inversion, rendimiento, años, ganancias) {
+        this.nombre = nombre;
+        this.inversion = inversion;
+        this.rendimiento = rendimiento;
+        this.años = años;
+        this.ganancias = ganancias;
     }
+}
+    
+const contenedorHistorial = document.getElementById("historial");
+const transacciones = JSON.parse(localStorage.getItem("transactionHistory")) || [];
+document.getElementById("botonCalcular").addEventListener('click',calcular);
+mostrarHistorial();
+
+function borrarHistorial() {
+    localStorage.clear("transactionHistory");
+}
+
+document.getElementById("borrarHistorial").addEventListener('click',borrarHistorial);
