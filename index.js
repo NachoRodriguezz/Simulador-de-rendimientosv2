d05729df2d3fe6c15db729f1e7fd846a28e2fc17
IgnacioRodriguez <rodriguezignaciodc@gmail.com>
2022-10-05 10:47:23 -0300

    cambios

--- index.js
+++ index.js
@@ -64,3 +64,18 @@ boton.addEventListener("click", () => {
     borrarHistorial();
 });
 
+//api cryptocurrensy precios
+
+    fetch('https://api.binance.com/api/v3/ticker/price')
+    .then( respuesta => respuesta.json() )
+    .then( datos => mostrarData(datos))
+    .catch( e => console.log(e))
+
+
+const mostrarData = (data)=>{
+    let body = ''
+    for (let i=0; i < data.length; i++) {
+        body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+    }
+    document.getElementById('data').innerHTML = body
+}    
