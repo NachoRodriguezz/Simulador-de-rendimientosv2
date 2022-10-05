d1d1f92740cd8d5d633477fedbeb57455d01f2a7
IgnacioRodriguez <rodriguezignaciodc@gmail.com>
2022-10-04 22:50:55 -0300

    cambios

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ function calcular() {
     let años = document.getElementById('años').value;
     let ganancias = (inversion * (rendimiento / 100) * años);
     document.getElementById('el-resultado').innerHTML = ganancias = (ganancias || "Tus ganancias son nulas");
-
     transacciones.push(new inversionTotal(nombre, inversion, rendimiento, años, ganancias))
     localStorage.clear();
     localStorage.setItem("transactionHistory", JSON.stringify(transacciones));
-    mostrarHistorial();
+    setTimeout(() => {
+        mostrarHistorial();
+    }, 3000);
     Toastify({
         text: "Su inversion a sido guardada en el historial!",
         duration: 3000
-      }).showToast();
+    }).showToast();
 };
 
-function mostrarHistorial(){
+function mostrarHistorial() {
     transacciones.forEach(item => {
         let itemHistorial = document.createElement("tr");
         itemHistorial.innerHTML = `
-        <tr><p>Invirtio en  ${item.nombre} /</p></tr>
-        <tr><p>Su inversion fe de $${item.inversion} /</p></tr>
-        <tr><p>Su rendimiento anual fue de %${item.rendimiento}/</p></tr>
-        <tr><p>Cantidad de años${item.años}/</p></tr>
-        <tr><p>Sus ganancias sin contar la inversion fue de $${item.ganancias}/</p></tr>`;
+        <tr><p>Invirtio en ${item.nombre}.</p></tr>
+        <tr><p>Su inversion fue de $${item.inversion}.</p></tr>
+        <tr><p>Su rendimiento anual fue de %${item.rendimiento}.</p></tr>
+        <tr><p>Cantidad de años ${item.años}.</p></tr>
+        <tr><p>Sus ganancias sin contar la inversion fue de $${item.ganancias}.</p></tr>
+        </hr>
+        `;
         contenedorHistorial.append(itemHistorial);
     });
 }
@@ -38,25 +41,26 @@ class inversionTotal {
         this.ganancias = ganancias;
     }
 }
-    
+
 const contenedorHistorial = document.getElementById("historial");
 const transacciones = JSON.parse(localStorage.getItem("transactionHistory")) || [];
-document.getElementById("botonCalcular").addEventListener('click',calcular);
+document.getElementById("botonCalcular").addEventListener('click', calcular);
 mostrarHistorial();
 
 function borrarHistorial() {
-    localStorage.clear("transactionHistory");
+    localStorage.clear();
+    transacciones = [];
+    JSON.stringify(localStorage.setItem(transacciones))
+    mostrarHistorial();
 }
-/* 
-document.getElementById("borrarHistorial").addEventListener('click',borrarHistorial);
- */
+
 let boton = document.getElementById("borrarHistorial");
 
 boton.addEventListener("click", () => {
-  Toastify({
-    text: "Su historial a sido borrado correctamente!",
-    duration: 5000
-  }).showToast();
-  borrarHistorial();
+    Toastify({
+        text: "Su historial a sido borrado correctamente!",
+        duration: 5000
+    }).showToast();
+    borrarHistorial();
 });
 
